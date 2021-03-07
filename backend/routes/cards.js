6b58df8ebb1d07d98a4a329441afed993e600f3a
Prabhu Sharan Singh <prabhu.sharan4@gmail.com>
2021-03-08 03:11:58 +0530

    Updated cards.js - added post route for /cards/:id/pay

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -13,10 +13,11 @@ const Transaction = require('../models/transactionSchema');
 
 // router for /cards
 router.route('/')
-  // Get list of cards associated for a user (GET /cards with userid)
+  // TODO - Get list of cards associated for a user (GET /cards with userid)
   .get(async(req, res)=> {
     res.status(200).json({msg:'Backend API route'});
   })
+
 
   // Add a credit card (POST /cards, this includes verification)
   .post(async(req, res)=> {
@@ -25,6 +26,8 @@ router.route('/')
     const cardNumber = req.body.cardNumber;
     const expiryMonth = req.body.expiryMonth;
     const expiryYear = req.body.expiryYear;
+    const outstandingAmount = req.body.outstandingAmount;
+    const creditLimit = req.body.creditLimit;
 
     // Check if length of card == 16 digit
     if (cardNumber.length != 16) {
@@ -47,7 +50,9 @@ router.route('/')
       name: cardName,
       account_number: cardNumber,
       expiry_month: expiryMonth,
-      expiry_year: expiryYear
+      expiry_year: expiryYear,
+      outstanding_amount: outstandingAmount,
+      credit_limit: creditLimit
     });
 
     // Save card in database and send response
@@ -66,30 +71,43 @@ router.route('/')
 ;
 
 
-//  ============= INCOMPLETE ROUTE
-
-// Payment of the bill (POST /cards/{id}/pay)
+// router for /cards/:id/pay
 router.route("/:id/pay")
+  // Payment of the bill (POST /cards/{id}/pay)
   .post(async(req, res)=> {
-    // save transaction in transactions_schema
+    // TODO - save transaction in transactions_schema
+
 
     // add/deduct from outstanding amount in card_schema
-    await Card.findOneAndUpdate({_id: req.params.id},
-      // Only for debits
-      {outstanding_amount: Number(foundCard.outstanding_amount) - Number(req.body.amount)},
-      function(err, foundCard) {
-        if(foundCard) {
-          res.status(200).send("Card found");
-        } else {
-          res.status(404).json({
-            status: "404",
-            message: "Card not found"
-          });
-        }
-      });
+    var id = req.params.id;
+    await Card.findById(id, function (err, foundCard) {
+        // If card is found in database
+        if (!err) {
+          // Check if transaction type is debit/credit
+          if(req.body.transactionType == "debit") {
+            foundCard.outstanding_amount -= Number(req.body.amount);
+          }
+          else if(req.body.transactionType == "credit") {
+            foundCard.outstanding_amount += Number(req.body.amount);
+          }
 
+          // Save updated card outstanding amount in database
+          foundCard.save((err) => {
+            if(!err) {
+              res.status(200).json({message: "Outstanding amount of card updated successfully"});
+            } else {
+              res.status(500).json({message: "Error saving updated outstanding amount"});
+            }
+         });
+        }
+        // If card is not found in database
+        else {
+          res.status(404).json({message: "Card not found"});
+        }
+    });
   })
 ;
+
 
 
 // Post statement (POST /cards/{id}/statements/{year}/{month}
