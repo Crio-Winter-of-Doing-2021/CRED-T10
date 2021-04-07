196cb01e57333ac5f3bc79383996fd7fc495817c
Prabhu Sharan Singh <prabhu.sharan4@gmail.com>
2021-04-07 22:03:55 +0530

    Added Rewards Functionality

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -8,19 +8,47 @@ const auth = require('../middleware/auth');
 
 // Card Collection
 const Card = require('../models/cardSchema');
+// Users Collection
+const User = require('../models/Users');
 
 // router for /cards/:id/pay
 router.post("/", auth, async(req, res)=> {
     // Get the url from :id parameter in server.js which is sent by user
     const id = req.originalUrl.split('/')[3];
-    await Card.findById(id, function (err, foundCard) {
+    await Card.findById(id, async (err, foundCard) => {
         // If card is found in database
         if (!err) {
+          const paymentAmount = req.body.amount;
           // Update outstanding amount on backend
-          foundCard.outstanding_amount -= Number(req.body.amount);
+          foundCard.outstanding_amount -= Number(paymentAmount);
+
+          // req.user.id is coming from auth middleware
+          const curLoggedInUserId = req.user.id;
+          console.log(curLoggedInUserId);
+
+          const user = await User.findById(curLoggedInUserId);
+
+          // Update reward points in user colection
+          if(paymentAmount > 4999) {
+            user.rewardPoints += paymentAmount * 2/100;
+          } else if(paymentAmount > 9999) {
+            user.rewardPoints += paymentAmount * 3/100;
+          } else if(paymentAmount > 19999) {
+            user.rewardPoints += paymentAmount * 3.5/100;
+          }
+
           // Save updated card outstanding amount in database
-          foundCard.save((err) => {
+          foundCard.save(async (err) => {
             if(!err) {
+              // Save user with updated reward points
+              await user.save((err, savedUser) => {
+                if(!err) {
+                  console.log('Reward Points updated successfully');
+                } else {
+                  console.log(err);
+                }
+              });
+
               res.status(200).json({message: "Bill paid successfully"});
             } else {
               res.status(500).json({message: "Error in paying bill"});
